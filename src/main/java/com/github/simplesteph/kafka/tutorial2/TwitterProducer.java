ff586ff60c9c3626a314d85706b9450c10239ab2
reidomath <mat.s.ferreira@gmail.com>
2020-08-16 12:13:14 -0300

    Alto Throughput para o producer

--- src/main/java/com/github/simplesteph/kafka/tutorial2/TwitterProducer.java
+++ src/main/java/com/github/simplesteph/kafka/tutorial2/TwitterProducer.java
@@ -132,6 +132,12 @@ public class TwitterProducer {
                 properties.setProperty(ProducerConfig.ACKS_CONFIG, "all");
                 properties.setProperty(ProducerConfig.RETRIES_CONFIG, Integer.toString(Integer.MAX_VALUE));
                 properties.setProperty(ProducerConfig.MAX_IN_FLIGHT_REQUESTS_PER_CONNECTION, "5");
+
+                // Alto Throughput para o producer - alto rendimento, compactando o envio das mensagens
+                properties.setProperty(ProducerConfig.COMPRESSION_TYPE_CONFIG, "snappy");
+                properties.setProperty(ProducerConfig.LINGER_MS_CONFIG, "20");
+                properties.setProperty(ProducerConfig.BATCH_SIZE_CONFIG, Integer.toString(32*1024));
+
         
                 // Criar o producer
                 KafkaProducer<String, String> producer = new KafkaProducer<>(properties);
